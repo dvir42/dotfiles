32861f93b561b7ecbd65aa823ab3b678aa12d730
dvir42 <dviros7@gmail.com>
2019-02-06 13:07:12 +0200

    emacs

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company intero haskell-mode which-key use-package try org-bullets linum-relative evil base16-theme))))
+    (helm-projectile projectile helm neotree web-mode general elpy company intero haskell-mode which-key use-package try org-bullets linum-relative evil base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
