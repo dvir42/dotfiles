3decd48348c6d811bc011b67038d36e247c447b6
dvir42 <dviros7@gmail.com>
2019-02-21 23:47:58 +0200

    i3 tablet stylus config

---
+++ i3/tablet.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Change DVI-I-1 to what monitor you want from running command: xrandr
+MONITOR="HDMI2"
+STYLUS_ID=`xinput | grep "Tablet stylus" | cut -f 2 | cut -c 4-5`
+
+xsetwacom --set $STYLUS_ID Button 2 "key e"
+
+xinput map-to-output $STYLUS_ID $MONITOR
